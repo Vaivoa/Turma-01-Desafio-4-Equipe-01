e3d3f8634879135899cc32e50a22f0692a7bf4e9
Yuri Dias <yuri.dias@vaivoa.com>
2021-09-21 15:27:59 -0300

    Feat: log service

---
+++ src/LogsVaivoa/Services/LogService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Dapper.Contrib.Extensions;
+
+namespace LogsVaivoa.Services
+{
+    public static class LogService
+    {
+        private static SqlConnection DbConnection()
+        {
+            return new SqlConnection(Environment.GetEnvironmentVariable("SqlConnection"));
+        }
+
+        public static (bool, object) InsertLog(LogModel log)
+        {
+            var errors = log.GetErrors();
+
+            if (errors.Any()) return (false, errors);
+
+            using var db = DbConnection();
+            db.Insert(log);
+
+            return (true, log);
+        }
+        
+        
+        
+    }
+}
