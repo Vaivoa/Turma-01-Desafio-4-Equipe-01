5a850d7cbba31d6c0b3a0a7fb3d8acbac384919c
Yuri Dias <yuri.dias@vaivoa.com>
2021-09-25 14:08:39 -0300

    Refactor: log model

--- src/LogsVaivoa/Models/Log.cs
+++ src/LogsVaivoa/Models/Log.cs
@@ -1,7 +1,36 @@
-﻿namespace LogsVaivoa.Models
+﻿using System.Collections.Generic;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace LogsVaivoa.Models
 {
     public class Log
     {
-        
+        public string Nome { get; set; }
+        public string Mensagem { get; set; }
+        public string Detalhe { get; set; }
+
+
+        public List<ValidationFailure> GetErrors() => new LogModelValidation().Validate(this).Errors;
+
     }
+
+    public class LogModelValidation : AbstractValidator<Log>
+    {
+        public LogModelValidation()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .MaximumLength(50).WithMessage("O campo {PropertyName} deve ter no maximo 50 caracteres");
+
+            RuleFor(c => c.Mensagem)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .MaximumLength(250).WithMessage("O campo {PropertyName} deve ter no maximo 50 caracteres");
+
+            RuleFor(c => c.Detalhe)
+                .MaximumLength(1000).WithMessage("O campo {PropertyName} deve ter no maximo 50 caracteres");
+
+        }
+    }
+    
 }
