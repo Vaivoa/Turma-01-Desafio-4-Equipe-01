d3383af6b2a022dd3d2389bd5d4b13f57d1bea46
Yuri Dias <yuri.dias@vaivoa.com>
2021-09-27 15:46:38 -0300

    Refactor

--- src/KafkaFunction/KafkaFunction/Startup.cs
+++ src/KafkaFunction/KafkaFunction/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 public class Startup : IWebJobsStartup
 {   
@@ -11,8 +12,8 @@ public class Startup : IWebJobsStartup
     {
         builder.Services.AddStackExchangeRedisCache(options =>
         {
-            options.InstanceName = "KafkaMessage";
-            options.Configuration = "kafkaredisvaivoa.redis.cache.windows.net:6380,password=1vwDwc3owbVeMgGyg5jSD2f6kB1xe2Wb1Ai1ovyPnLY=,ssl=True,abortConnect=False";
+            options.InstanceName = Environment.GetEnvironmentVariable("InstanceName");
+            options.Configuration = Environment.GetEnvironmentVariable("RedisConnection");
         });
     }
 }
